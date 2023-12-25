00d0abf354d7da6373323aed4372f9c984bc8d91
dsila2017 <you@example.com>
2023-12-25 05:32:36 +0400

    Update

--- SwiftUI Animations/SwiftUI Animations/ContentView.swift
+++ SwiftUI Animations/SwiftUI Animations/ContentView.swift
@@ -6,22 +6,34 @@
 //
 
 import SwiftUI
+import EffectsLibrary
 
 struct ContentView: View {
     @State var isAnimated: Bool = false
     var body: some View {
-        VStack {
+        
+        ZStack {
             
-            Text("TBC IT Academy")
-                .scaleEffect(isAnimated ? 2 : 1)
-                .foregroundStyle(isAnimated ? .red : .blue)
-                .bold()
-                .onTapGesture {
-                    isAnimated.toggle()
-                }
-                .animation(.easeInOut(duration: 4).repeatForever(), value: isAnimated)
+            SnowView()
+            
+            FireworksView()
+            
+            VStack {
+                
+                Text("TBC IT Academy")
+                    .scaleEffect(isAnimated ? 2 : 1)
+                    .foregroundStyle(isAnimated ? .red : .blue)
+                    .bold()
+                    .onTapGesture {
+                        isAnimated.toggle()
+                    }
+                    .animation(.easeInOut(duration: 4).repeatForever(), value: isAnimated)
+                
+                Tree()
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
